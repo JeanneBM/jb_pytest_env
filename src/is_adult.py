1318b3315dd82cd0ac52768aec67d0d7c8500613
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2021-04-13 00:14:26 +0200

    Update is_adult.py

--- src/is_adult.py
+++ src/is_adult.py
@@ -1,15 +1,3 @@
 def is_adult (age: int) -> bool:
   pass
 
-def test_is_adult():
-  
-  #given
-  age = 18
-  
-  #when
-  result = is_adult(age)
-  
-  #then
-  assert result
-  assert is_adult(20)
-  
