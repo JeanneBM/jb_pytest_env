36d61f4c18f3c7bf6d61c757fe10bfe856fa9a1c
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2021-04-14 23:46:13 +0200

    Create bank.py

---
+++ src/bank.py
@@ -0,0 +1,13 @@
+class bank:
+    def __init__(self):
+        self.amount = 0
+
+    def add_money(self, money: int):
+        self.amount += money
+
+    def withdraw_money(self, money):
+        self.amount -= money
+
+        return money
+
+
