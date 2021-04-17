46d09caaaf3eee9db66d22d2590b368a3461bf76
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2021-04-18 00:34:03 +0200

    Update selenium_test.py

--- tests/selenium_test.py
+++ tests/selenium_test.py
@@ -1,7 +1,10 @@
 from selenium import webdriver
 from webdriver_manager.firefox import GeckoDriverManager
 
-driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
-driver.get("http://www.python.org")
+options = webdriver.FirefoxOptions()
+options.headless = True
+options.add_argument('--no-sandbox')
+driver = webdriver.Firefox(options=options, executable_path='geckodriver')
+driver.get('https://www.google.com/')
 
 driver.close()
