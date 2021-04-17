53ef769b7d9fce13e830827210e9fa20d82fa157
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2021-04-18 00:08:31 +0200

    Update selenium_test.py

--- tests/selenium_test.py
+++ tests/selenium_test.py
@@ -1,7 +1,6 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
+from webdriver_manager.firefox import GeckoDriverManager
 
-driver = webdriver.Firefox()
-
+driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
 
 driver.close()
