eda7bd73a38ce3e1385992d2b06b7bb5286af40c
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2021-04-18 02:47:03 +0200

    Update selenium_test.py

--- tests/selenium_test.py
+++ tests/selenium_test.py
@@ -1,12 +1,4 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-import geckodriver_autoinstaller
-
-
-geckodriver_autoinstaller.install()  # Check if the current version of geckodriver exists
-                                     # and if it doesn't exist, download it automatically,
-                                     # then add geckodriver to path
 
 driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
