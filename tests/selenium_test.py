0e5c10c1ba402f9f1f5299767a97edccbeb9de48
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2021-04-18 02:13:53 +0200

    Update selenium_test.py

--- tests/selenium_test.py
+++ tests/selenium_test.py
@@ -1,11 +1,6 @@
 from selenium import webdriver
-import geckodriver_autoinstaller
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
+binary = FirefoxBinary('/home/travis/virtualenv/python3.8.7/lib/python3.8/site-packages/selenium/webdriver/firefox')
+browser = webdriver.Firefox(firefox_binary=binary)
 
-geckodriver_autoinstaller.install()  # Check if the current version of geckodriver exists
-                                     # and if it doesn't exist, download it automatically,
-                                     # then add geckodriver to path
-
-driver = webdriver.Firefox()
-driver.get("http://www.python.org")
-assert "Python" in driver.title
