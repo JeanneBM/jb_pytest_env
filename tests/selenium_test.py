d2f1e36ff1f12034588775187605c688a714b3ae
JeanneBM <55690923+JeanneBM@users.noreply.github.com>
2021-04-19 23:50:39 +0200

    Update selenium_test.py

--- tests/selenium_test.py
+++ tests/selenium_test.py
@@ -5,7 +5,8 @@ from selenium import webdriver
 display = Display(visible=0, size=(800, 600))
 display.start()
 
-browser = webdriver.Firefox(home/geckodriver)
+browser = webdriver.Firefox()
 browser.get('http://www.python.org')
-'''
+
 browser.close()
+'''
